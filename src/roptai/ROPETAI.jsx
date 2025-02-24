e7d9c5c003dca68b5e2b635f05f445efe000330d
AlexanderJAON <jhon.osma@correounivalle.edu.co>
2025-02-24 13:50:02 -0500

    first commit

---
+++ src/roptai/ROPETAI.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function ROPETAI() {
+  const [selectedOpction, setSelectedOption] = useState("null");
+
+  const handleSelection = (option) => {
+    setSelectedOption(option);
+    console.log("Opcion seleccionada:", option);
+  };
+  return (
+      <main className="inspection-container">
+        <div className="inspection-question">
+          <p>
+            Verificar que la bomba dosificadora de cloro para el agua oxidada
+            esté encendida
+          </p>
+        </div>
+        <div className="response-buttons">
+          <button onClick={() => handleSelection("No se encontró anomalía")}>
+            No se encontró anomalía
+          </button>
+          <button onClick={() => handleSelection("Se encontró anomalía")}>
+            Se encontró anomalía
+          </button>
+          <button onClick={() => handleSelection("No se realizó")}>
+            No se realizó
+          </button>
+        </div>
+      </main>
+  );
+}
+export default ROPETAI;
---
+++ src/routers/Router.jsx
@@ -0,0 +1,119 @@
+import { createBrowserRouter } from "react-router-dom";
+import PublicRoute from "./PublicRoute";
+import Login from "../pages/login/Login";
+import ProtectedRoute from "./ProtectedRoute";
+import Home from "../pages/home/Home";
+import Acidification from "../pages/acidification/Acidification";
+import Shortage from "../pages/shortage/Shortage";
+import Pollution from "../pages/pollution/Pollution";
+import Sensitization from "../pages/acidification/Sensitization";
+import Awareness from "../pages/shortage/Awareness";
+import GarbageScene from "../pages/pollution/GarbageScene";
+import Menu from "../components/Menu";
+import Quiz from "../pages/quiz/Quiz";
+import SpillScene from "../pages/pollution/SpillScene";
+import DeforestationScene from "../pages/pollution/DeforestationScene";
+
+
+const Router = createBrowserRouter([
+    {
+        path: "/", 
+        element: (
+            <PublicRoute><Login /></PublicRoute>
+        )
+    },
+    {
+        path: "/home",
+        element: (
+            <>
+                <Menu />
+                <ProtectedRoute><Home /></ProtectedRoute>
+            </>
+        )
+    },
+    {
+        path: "/acidification",
+        element: (
+            <>
+                <Menu />
+                <ProtectedRoute><Acidification /></ProtectedRoute>
+            </>
+        )
+    },
+    {
+        path: "/shortage",
+        element: (
+            <>
+                <Menu />
+                <ProtectedRoute><Shortage /></ProtectedRoute>
+            </>
+        )
+    },
+    {
+        path: "/pollution",
+        element: (
+            <>
+                <Menu />
+                <ProtectedRoute><Pollution /></ProtectedRoute>
+            </>
+        )
+    },
+    {
+        path: "/sensitization",
+        element: (
+            <>
+                <Menu />
+                <ProtectedRoute><Sensitization /></ProtectedRoute>
+            </>
+        )
+    },
+    {
+        path: "/awareness",
+        element: (
+            <>
+                <Menu />
+                <ProtectedRoute><Awareness /></ProtectedRoute>
+            </>
+        )
+    },
+    {
+        path: "/GarbageScene",
+        element: (
+            <>
+                <Menu />
+                <ProtectedRoute><GarbageScene /></ProtectedRoute>
+            </>
+        )
+    },
+
+    {
+        path: "/quiz",
+        element: (
+            <>
+                <Menu />
+                <ProtectedRoute><Quiz/></ProtectedRoute>
+            </>
+        )
+    },
+    {
+        path: "/SpillScene",
+        element: (
+            <>
+                <Menu />
+                <ProtectedRoute><SpillScene/></ProtectedRoute>
+            </>
+        )
+    },
+    {
+        path: "/DeforestationScene",
+        element: (
+            <>
+                <Menu />
+                <ProtectedRoute><DeforestationScene/></ProtectedRoute>
+            </>
+        )
+    },
+
+]);
+
+export default Router;
