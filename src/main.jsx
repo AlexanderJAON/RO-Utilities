fe360ecb904b6142d5daf4e5f4c4f67fc8fee605
AlexanderJAON <jhon.osma@correounivalle.edu.co>
2025-02-24 15:14:54 -0500

    Ptai

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import ROPETAI from './roptai/ROPETAI.jsx'
+import { RouterProvider } from 'react-router-dom'
+import Router from './routers/Router.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ROPETAI/>
+    <RouterProvider router={Router} />
   </StrictMode>,
 )
+
