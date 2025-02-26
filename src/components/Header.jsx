42d4995d6c5f88f4a514d702b9d46f59c662e5dd
AlexanderJAON <jhon.osma@correounivalle.edu.co>
2025-02-25 19:46:19 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,6 +15,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo" onClick={goToHome}><img src="/images/ingredion.png" alt="" /></div>
+      <div className="htittle">Utilities</div>
       <nav className="menu">
         <button className="menu-button" onClick={() => setMenuOpen(!menuOpen)}>☰</button>
         {menuOpen && (
