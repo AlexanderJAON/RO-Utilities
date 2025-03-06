1c6a7811525a3d38ef9b98cd90f3879ec3500571
AlexanderJAON <jhon.osma@correounivalle.edu.co>
2025-03-06 14:13:36 -0500

    Update useInspectionQuestions.js

--- src/hooks/useInspectionQuestions.js
+++ src/hooks/useInspectionQuestions.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 import { generateExcel, sendExcelByEmail } from "./exportToExcel";
 
 const useInspectionQuestions = (initialQuestions, operatorName, shift) => {
@@ -7,25 +6,30 @@ const useInspectionQuestions = (initialQuestions, operatorName, shift) => {
   const [responses, setResponses] = useState([]);
   const [inspectionCompleted, setInspectionCompleted] = useState(false);
 
-
+  // Obtener la fecha actual en formato YYYY-MM-DD
+  const getCurrentDate = () => {
+    const today = new Date();
+    return today.toISOString().split("T")[0]; // Extrae solo la parte de la fecha
+  };
 
   const handleSelection = (option, anomalyDescription = "", noticeGiven = "") => {
     const newResponse = {
+      date: getCurrentDate(), // Agrega la fecha a cada respuesta
       question: initialQuestions[currentQuestionIndex],
       response: option,
       anomalyDescription: option === "Se encontró anomalía" ? anomalyDescription : "",
       noticeGiven: option === "Se encontró anomalía" ? noticeGiven : "",
     };
-  
-    const updatedResponses = [...responses, newResponse]; // ✅ Incluye la última respuesta
-  
+
+    const updatedResponses = [...responses, newResponse];
+
     setResponses(updatedResponses);
-  
+
     if (currentQuestionIndex < initialQuestions.length - 1) {
       setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
     } else {
       setInspectionCompleted(true);
-  
+
       generateExcel(updatedResponses, operatorName, shift)
         .then((filePath) => {
           sendExcelByEmail(filePath);
@@ -33,7 +37,6 @@ const useInspectionQuestions = (initialQuestions, operatorName, shift) => {
         .catch((error) => console.error("❌ Error generando/enviando el Excel:", error));
     }
   };
-  
 
   return {
     currentQuestion: initialQuestions[currentQuestionIndex],
